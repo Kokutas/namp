59eafd8bdbc41c58502e75e54c4c6c7d50c5e2f2
kokutas <2150748714@qq.com>
2020-10-30 22:37:36 +0800

    first commit of network's adaptor infomation'

--- client/web/main.go
+++ client/web/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"nmap/client/web/service/network"
+)
+
 func main() {
-	
+	adaptors,err:=network.Adaptors()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, adaptor := range adaptors {
+		log.Println(i,adaptor)
+	}
 }
--- server/web/main.go
+++ server/web/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"nmap/server/web/service/network"
+)
+
 func main() {
-	
+	adaptors,err:=network.Adaptors()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, adaptor := range adaptors {
+		log.Println(i,adaptor)
+	}
 }
